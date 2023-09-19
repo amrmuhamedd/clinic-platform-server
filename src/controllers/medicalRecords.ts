0d5ebe96e769a0726d6541fdabb1063da6cdd81c
Amr Mohamed <amrmuhmedd@gmail.com>
2023-09-20 01:46:02 +0300

    create list medical record apis

--- src/controllers/medicalRecords.ts
+++ src/controllers/medicalRecords.ts
@@ -25,3 +25,33 @@ export const createMedicalRecord = async (req: Request, res: Response) => {
     res.status(500).json({ success: false, error: "Internal server error" });
   }
 };
+
+export const listMedicalRecords = async (req: Request, res: Response) => {
+  try {
+    const page = parseInt(req.query.page as string) || 1; // Default to page 1
+    const perPage = parseInt(req.query.perPage as string) || 10; // Default to 10 records per page
+
+    // Calculate skip and limit values for pagination
+    const skip = (page - 1) * perPage;
+    const limit = perPage;
+
+    // Query the database for medical records with pagination
+    const medicalRecords = await MedicalRecordModel.find()
+      .populate({ path: "doctorId", select: "_id name" })
+      .skip(skip)
+      .limit(limit);
+
+    // Count the total number of records (useful for pagination)
+    const totalCount = await MedicalRecordModel.countDocuments();
+
+    res.status(200).json({
+      data: medicalRecords,
+      page,
+      perPage,
+      totalCount,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: "Internal server error" });
+  }
+};
--- src/routes/medicalRecords.ts
+++ src/routes/medicalRecords.ts
@@ -6,7 +6,10 @@ import ensureAuthenticatedUser from "../middlewares/ensureAuthenticatedUser";
 import { validateUserRole } from "../middlewares/validateRole";
 import { UserRolesEnum } from "../enums/userRole.enum";
 import { validateMedicalRecord } from "../validations/validateCreateMedicalRecord";
-import { createMedicalRecord } from "../controllers/medicalRecords";
+import {
+  createMedicalRecord,
+  listMedicalRecords,
+} from "../controllers/medicalRecords";
 
 const router = express.Router();
 
@@ -67,4 +70,72 @@ router.post(
   createMedicalRecord
 );
 
+/**
+ * @swagger
+ * /api/records/list:
+ *   get:
+ *     summary: List all medical records with pagination
+ *     tags:
+ *       - Medical Records
+ *     security:
+ *       - bearerAuth: [] # Use the security scheme defined below
+ *     parameters:
+ *       - name: page
+ *         in: query
+ *         description: The page number to retrieve (default 1)
+ *         required: false
+ *         schema:
+ *           type: integer
+ *           default: 1
+ *       - name: perPage
+ *         in: query
+ *         description: The number of records per page (default 10)
+ *         required: false
+ *         schema:
+ *           type: integer
+ *           default: 10
+ *     responses:
+ *       '200':
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       notes:
+ *                         type: string
+ *                         example: Patient presented with flu-like symptoms.
+ *                       message:
+ *                         type: string
+ *                         example: Prescribed medication for treatment.
+ *                       diagnosis:
+ *                         type: string
+ *                         example: Influenza
+ *                       session_date:
+ *                         type: string
+ *                         format: date-time
+ *                         example: 2023-09-20T12:00:00.000Z
+ *                 page:
+ *                   type: integer
+ *                 perPage:
+ *                   type: integer
+ *                 totalCount:
+ *                   type: integer
+ *       '500':
+ *         description: Internal server error
+ */
+
+router.get(
+  "/list",
+  ensureAuthenticatedUser,
+  validateUserRole([UserRolesEnum.Doctor]),
+  listMedicalRecords
+);
 export default router;
